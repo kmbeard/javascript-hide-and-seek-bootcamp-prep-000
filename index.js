18ced5385698174d74a3f4775e318e2472251111
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-07 04:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const rankedList = document.querySelectorAll('.ranked-list li')
   for (var i = 0; i < rankedList; i++) {
-    
+    rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + n
   }
 }
 
