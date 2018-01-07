92ae8c6f6e8af88e5f6e7b3a6e77e8610abe99a7
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-07 03:55:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function deepestChild() {
   var grandNode = document.querySelector('#grand-node')
   var deep = grandNode.querySelectorAll('div')
-  deep = deep[deep.length -1]
+  deep = deep[deep.length - 1]
   return deep
 }
